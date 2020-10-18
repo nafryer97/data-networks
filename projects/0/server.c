4f0df0cad23455a5e595831ec9e7869d7f08ebef
nafryer97 <nafryer97@gmail.com>
2020-10-18 18:44:05 -0500

    changing project 0 files back to their originals from the handout.

--- projects/0/server.c
+++ projects/0/server.c
@@ -1,55 +1,47 @@
 #include"netinet/in.h" 
 #include"sys/socket.h"
-#include"stdio.h" 
-#include"stdlib.h"
+#include"stdio.h"
 #include"string.h"
 #include"time.h"
 
 main( )
 {
-    struct sockaddr_in sa;
-    struct sockaddr_in cli;
-    int sockfd,conntfd;
-    int len,ch;
-    char str[100]; 
-    time_t tick;
-    
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
+	struct sockaddr_in sa;
+	struct sockaddr_in cli;
+	int sockfd,conntfd;
+	int len,ch;
+	char str[100]; 
+	time_t tick;
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
 
-    if(sockfd<0)
-    {
-        printf("error in socket\n");
-        exit(0);
-    }
-    else 
-    {
-        printf("\nSocket opened");
-    }
+	if(sockfd<0)
+	{
+		printf("error in socket\n");
+		exit(0);
+	}
+	else printf("\nSocket opened");
 
-    bzero(&sa,sizeof(sa));
+	bzero(&sa,sizeof(sa));
+	sa.sin_port=htons(2100);
+	sa.sin_addr.s_addr=htonl(0);
+	
+	if(bind(sockfd,(struct sockaddr*)&sa,sizeof(sa))<0)
+	{
+		printf("Error in binding\n");
+	}
+	else
+	printf("\nBinded Successfully");
 
-    sa.sin_port=htons(50051);
+	listen(sockfd,50);
 
-    sa.sin_addr.s_addr=htonl(0);
-
-    if(bind(sockfd,(struct sockaddr*)&sa,sizeof(sa))<0)
-    {
-        printf("Error in binding\n");
-    }
-    else
-    {
-        printf("\nBound Successfully");
-    }
-    listen(sockfd,50);
-
-    for(;;)
-    {
-        len=sizeof(ch);
-        conntfd=accept(sockfd,(struct sockaddr*)&cli,&len); printf("\nAccepted");
-        tick=time(NULL);
-        snprintf(str,sizeof(str),"%s",ctime(&tick));
-
-        //printf("%s",str);
-        write(conntfd,str,100);
-    }
+	for(;;)
+	{
+		len=sizeof(ch);
+		conntfd=accept(sockfd,(struct sockaddr*)&cli,&len); 
+		printf("\nAccepted");
+		tick=time(NULL);
+		snprintf(str,sizeof(str),"%s",ctime(&tick));
+		//printf("%s",str);
+		write(conntfd,str,100);
+	}
 }
